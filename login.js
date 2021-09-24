6d3e2c185e15e7df3a16404a1eb8d6f414f304e0
Ritvik Github <89140107+Ritvik-Github@users.noreply.github.com>
2021-09-24 17:49:09 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,17 @@
+//Firebase Database (Please Do not share this to anyone who is not a developer of this website);
+const firebaseConfig = {
+  apiKey: "AIzaSyABCnaN2CP7at_U_7c9XDxoGY7V_uEhl4Y",
+  authDomain: "tree-chat-db.firebaseapp.com",
+  databaseURL: "https://tree-chat-db-default-rtdb.firebaseio.com",
+  projectId: "tree-chat-db",
+  storageBucket: "tree-chat-db.appspot.com",
+  messagingSenderId: "613108623407",
+  appId: "1:613108623407:web:74d4726c4c8d4c16073eda"
+};
+
+function adduser() {
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+    this.location = "room.html";
+    // "this" is actually what you are looking at like when you are in a website it shows the window and "this" variable automatically assigns itself to window.
+}
---
+++ room.js
@@ -0,0 +1,14 @@
+//Firebase Database (Please Do not share this to anyone who is not a developer of this website);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABCnaN2CP7at_U_7c9XDxoGY7V_uEhl4Y",
+  authDomain: "tree-chat-db.firebaseapp.com",
+  databaseURL: "https://tree-chat-db-default-rtdb.firebaseio.com",
+  projectId: "tree-chat-db",
+  storageBucket: "tree-chat-db.appspot.com",
+  messagingSenderId: "613108623407",
+  appId: "1:613108623407:web:74d4726c4c8d4c16073eda"
+};
+
+const app = initializeApp(firebaseConfig);
